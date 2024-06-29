82f7de5dc65420df9259a1a5034f466abe99caba
Adrian Ciolek <ciolek.adrian@protonmail.com>
2024-06-29 13:10:13 +0200

    Added logs generate option

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,15 @@
 
 if [ "$1" = "--date" ]; then
     date +"%Y-%m-%d"
+
+elif [ "$1" = "--logs" ]; then
+    for (( i=1; i<=100; i++ )); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data utworzenia: $(date '+%Y-%m-%d %H:%M:%S')" >> "$filename"
+    done
 else
-    echo "Nieprawidłowa opcja. Użycie: skrypt.sh --date"
+    echo "Nieprawidłowa opcja. Użycie:"
     exit 1
 fi
